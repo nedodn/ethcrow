999aa245c9715c6fdddee8b31dde76e4082f502d
nedodn <odennate@gmail.com>
2017-09-28 14:39:15 -0500

    Add files via upload

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -247,15 +247,15 @@ window.refund = function(){
 }
 
 $( document ).ready(function() {
-  if (typeof web3 !== 'undefined') {
+  // if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
     //Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
+  // } else {
+  //   console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+  //   // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+  //   window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  // }
 
   Escrow.setProvider(web3.currentProvider);
 });
